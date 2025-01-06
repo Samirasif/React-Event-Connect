4e1a101ff36826ed2ad64b4c580c58c7b7b21e55
Md Abdul Kader <devabdulemail@gmail.com>
2025-01-06 23:38:32 +0600

    fixed

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,26 +1,22 @@
 import React from "react";
-<<<<<<< HEAD
-
-const Home = () => {
-  return <div></div>;
-=======
 import Hero from "../Hero/Hero";
 import FindRightOne from "../FindRightOne/FindRightOne";
 import HowItWork from "../HowItWork/HowItWork";
 import FeaturedPost from "../FeaturedPost/FeaturedPost";
 import ContactUs from "../ContactUs/ContactUs";
+import Banner from "../Banner/Banner";
 
 const Home = () => {
   return (
     <div className="">
       <Hero />
+      <Banner />
       <FindRightOne />
       <HowItWork />
       <FeaturedPost />
       <ContactUs />
     </div>
   );
->>>>>>> b0ad5e7 (feat:filter page)
 };
 
 export default Home;
